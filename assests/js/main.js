2fe0c50f93c2ceb1466bbf964244f673df26f851
olawale-oloye <olawaleoloye@gmail.com>
2023-08-12 20:07:36 +1000

    JS updated

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,20 +1,15 @@
 // typed effect
-let typed = new Typed(".typing-effect", {
-  strings: ["Talent", "Hire", "Recruit", "Match", "Job", "Team"],
-  backSpeed: "9",
-  typeSpeed: "9",
-  loop: true,
-});
 
-const nav = document.querySelector("nav .ul-links  ul");
+const nav = document.querySelector("nav .ul-links");
 const tog = document.querySelector(".toggler");
 
 tog.addEventListener("click", function () {
-  if ((nav.style.top = "-50%")) {
-    nav.style.top = "8%";
-    console.log(nav.style.top);
-  } else if ((nav.style.top = "8%")) {
-    nav.style.top = "-50%";
-    console.log(nav.style.top);
-  }
+  nav.classList.toggle("show-nav");
+  console.log("hello");
+});
+
+document.querySelectorAll("nav .ul-links ul li").forEach((links) => {
+  links.addEventListener("click", function () {
+    nav.classList.remove("show-nav");
+  });
 });
